aee9e50dade721971848e0700b69098bb2d969cc
Vinicios <vinicioscarvalho.eng@gmail.com>
2026-02-27 11:02:31 -0300

    Automacao: Adicao da view VW_PERCENTUAL_VARIACAO_FATURAMENTO no Projeto Hospitalar 03 27/02/2026 11:02:31,52

--- 03_Projeto_Hospitalar/atualizar_projeto.bat
+++ 03_Projeto_Hospitalar/atualizar_projeto.bat
@@ -1,7 +1,7 @@
 @echo off
 echo Iniciando atualizacao do Portfolio...
 git add .
-git commit -m "Automacao: Atualizacao do Projeto Hospitalar 03 %date% %time%"
+git commit -m "Automacao: Adicao da view VW_PERCENTUAL_VARIACAO_FATURAMENTO no Projeto Hospitalar 03 %date% %time%"
 git push origin main
 echo.
 echo Tudo certo! Seu projeto ja esta no GitHub.
